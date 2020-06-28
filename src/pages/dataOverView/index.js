ddb8246f095ee7988b32ac0535a39a7558b558ab
yambo1992 <1810962886@qq.com>
2020-06-28 20:41:09 +0800

    tree2

--- src/pages/dataOverView/index.js
+++ src/pages/dataOverView/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Tabs, Row, Col } from 'antd'
-import { DemoTree } from './components/TreeCom'
+import { DemoTree, DragTree } from './components/TreeCom'
 const { TabPane } = Tabs;
 
 const DatasOverView = () => {
@@ -11,6 +11,7 @@ const DatasOverView = () => {
             <TabPane tab="tree" key='1'>
                 <Row>
                     <Col span={24}><DemoTree /></Col>
+                    <Col span={24}><DragTree /></Col>
                 </Row>
             </TabPane>
            
